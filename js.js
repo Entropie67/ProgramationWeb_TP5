0e8fdf09a84cf5544bfcae73a1f98a22f09f9685
NN-Guillaume <olivier.elophe@gmail.com>
2019-03-27 12:31:18 +0100

    Suite

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
     $("p").click(function(){
-        alert($(this).text());
+        alert($(this).html());
+    });
+    $(".disparaitre").hide(1000,function () {
+        $(".disparaitre").text("Le texte est modifié !!!");
     });
 
+    $(".disparaitre").show(1000);
+    /*
     $(".element").hide(1000, function () {
         alert("coucou");
 
-    });
+    }); */
 
 });
 
